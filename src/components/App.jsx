c2e722dd024e32bd94523cf2a5e981f7a2f16d6f
Natalia Karpenko <106306905+natalia-no-war@users.noreply.github.com>
2023-03-08 22:22:32 +0200

    added methods

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -42,6 +42,21 @@ export class App extends Component {
     );
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     return (
       <>
@@ -55,7 +70,6 @@ export class App extends Component {
 
         <ContactsList
           onContactDelete={this.contactDeleteHandler}
-          // contacts={this.state.contacts}
           contacts={this.getFilteredContacts()}
         />
       </>
